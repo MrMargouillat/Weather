364f2f4469731c2d930737e65fb6334aab609cd4
MrMargouillat <klaja.raphael@gmail.com>
2016-10-27 18:29:29 +0200

    Final

--- index.php
+++ index.php
@@ -5,28 +5,75 @@
 		<meta charset="UTF-8">
 		
 		<meta content="width=device-width, initial-scale=0.25, maximum-scale=0.25,minimum-scale=0.25, user-scalable=0" name="viewport">
-
+		<meta http-equiv="pragma" content="no-cache" />
 		<link href="css/style.css" rel="stylesheet">
+		<link href="style.css" rel="stylesheet">
 	</head>
-	<body class="hideOverflow">
-		<header>
-			<nav class="nav">
-				<a class="btnNav" href="index.html">home</a>
-				<a class="btnNav" href="about.html">my work</a>
-				<a class="btnNav language_menu" href="#" id="header__language" >language</a>
-					<ul class="nav_child">
-						<li><a href="">English</a></li>
-						<li><a href="">French</a></li>
-						<li><a href="">German</a></li>
-					</ul>
-			</nav>
-		</header>
-			<div class="imgBackStanding"></div>
-			<h1 class="welcomeMsg">
-				<p>Welcome to Website,</p>
-				<p>the portfolio of a </p>
-				<p>16-year-old coder</p>
-			</h1>
+	<body>
+		<div class="container">
+		</div>
+				<div class="containerWork">
+					<div class="cardLight">
+							<div class="cardImgContainer">
+								<img class="cardImg" src="./img/Newcastle.jpg" alt="">
+							</div>
+							<div class="cardContainer" id="newcastle">
+								<h3 class="cardTitle">Newcastle</h3>
+								<div class="cardTemperature" >11°C</div>
+								<div class="cardWord"></div>
+								<div class="logoWeather">F</div>
+							</div>
+						</div>
+
+						<div class="cardLight">
+							<div class="cardImgContainer">
+								<img class="cardImg" src="./img/Paris.jpg" alt="">
+							</div>
+							<div class="cardContainer" id="paris">
+								<h3 class="cardTitle">Paris</h3>
+								<div class="cardTemperature" >11°C</div>
+								<div class="cardWord"></div>
+								<div class="logoWeather">F</div>
+							</div>
+						</div>
+						
+						<div class="cardLight">
+							<div class="cardImgContainer">
+								<img class="cardImg" src="./img/Norden.jpg" alt="">
+							</div>
+							<div class="cardContainer"  id="norden">
+								<h3 class="cardTitle">Norden</h3>
+								<div class="cardTemperature">6°C</div>
+								<div class="cardWord"></div>
+								<div class="logoWeather">F</div>
+								
+							</div>
+						</div>
+						<div class="cardLight">
+							<div class="cardImgContainer">
+								<img class="cardImg" src="./img/Marseille.jpg" alt="">
+							</div>
+							<div class="cardContainer" id="marseille">
+								<h3 class="cardTitle">Marseille</h3>
+								<div class="cardTemperature" >Chargement...</div>
+								<div class="cardWord"></div>
+								<div class="logoWeather">F</div>
+								
+							</div>
+						</div>
+						<div class="cardLight">
+							<div class="cardImgContainer">
+								<img class="cardImg" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/148866/thumb.jpg" alt="">
+							</div>
+							<div class="cardContainer" id="krakow">
+								<h3 class="cardTitle">Cracovie</h3>
+								<div class="cardTemperature" >12°C</div>
+								<div class="cardWord"></div>
+								<div class="logoWeather">F</div>								
+							</div>
+						</div>
+						
+					</div>
 		<footer>
 			<p class="pCredits">Big thanks to <strong class="credits">Héloise Guillot</strong></p>
 		</footer>
--- js/app.js
+++ js/app.js
@@ -1,6 +1,117 @@
-var navBtn  = document.querySelector('#header__language')
-navBtn.addEventListener("click", function(e){
-    e.preventDefault();
-    console.log(document.querySelector('body'));
-    document.querySelector('body').classList.toggle('with--sidebar');
-});
+window.onload = function(){
+	var getHttpRequest = function () {
+		var httpRequest = false;
+
+		if (window.XMLHttpRequest) { // Mozilla, Safari,...
+		httpRequest = new XMLHttpRequest();
+		if (httpRequest.overrideMimeType) {
+			httpRequest.overrideMimeType('text/xml');
+		}
+		}
+		else if (window.ActiveXObject) { // IE
+		try {
+			httpRequest = new ActiveXObject("Msxml2.XMLHTTP");
+		}
+		catch (e) {
+			try {
+			httpRequest = new ActiveXObject("Microsoft.XMLHTTP");
+			}
+			catch (e) {}
+		}
+		}
+
+		if (!httpRequest) {
+		alert('Abandon :( Impossible de créer une instance XMLHTTP');
+		return false;
+		}
+
+		return httpRequest
+}
+
+function requestWeater(elem , str){
+	var xhr = getHttpRequest();
+	xhr.open("GET","http://api.openweathermap.org/data/2.5/weather?q="+ str +"&appid=bb2ca672d35f1428507d1b4907c03cfa&units=metric&&lang=fr", true);
+	xhr.send();
+	xhr.onreadystatechange = function(){
+		if(xhr.readyState == 4)
+		{
+			if(xhr.status === 200)
+			{
+				 parse = JSON.parse(xhr.responseText);
+
+				 elem.querySelector(".cardTitle").innerText = parse.name
+
+				 elem.querySelector(".cardTemperature").innerText = parse.main.temp + " °C";
+
+				 elem.querySelector(".cardWord").innerText = parse.weather[0].description;	
+
+
+				var id = parse.weather[0].id;
+				/*
+				https://openweathermap.org/weather-conditions
+				api infos 
+				*/
+				if(id >= 200 && id <300 || id >= 520 && id < 600)
+				{
+					elem.querySelector(".logoWeather").innerText = "Q"	
+				}
+				else if(id >= 300 && id < 400)
+				{
+					elem.querySelector(".logoWeather").innerText = "U"	
+				}
+				else if(id >= 500 && id < 505)
+				{
+					elem.querySelector(".logoWeather").innerText = "J"
+				}
+				else if(id == 511 || id >= 600 && id < 700)
+				{
+					elem.querySelector(".logoWeather").innerText = "I"
+				}
+				else if(id >= 700 && id < 800	)
+				{
+					elem.querySelector(".logoWeather").innerText = "B"
+				}
+				else if(id === 800)
+				{
+					elem.querySelector(".logoWeather").innerText = "1"
+				}
+				else if(id === 801)
+				{
+					elem.querySelector(".logoW	eather").innerText = "A"
+				}
+				else if(id === 802)
+				{
+					elem.querySelector(".logoWeather").innerText = "3"
+				}
+				else if(id === 803|| id === 804)
+				{
+					elem.querySelector(".logoWeather").innerText = "3"
+				}
+				else
+				{
+					elem.querySelector(".logoWeather").innerText = ":"
+				}
+			}
+			else
+			{
+				alert("Erreur");
+			}
+		}
+		else
+		{
+			//elem.innerText += "Chargement...";
+		}
+  }
+}
+
+var NC = document.getElementById('newcastle')
+var NCjson = requestWeater(NC, "Newcastle,uk");
+var paris = document.getElementById('paris')
+var parisjson  = requestWeater(paris, "Paris,fr");
+var mar = document.getElementById('marseille')
+var marjson  = requestWeater(mar, "Marseille,fr");
+var krk = document.getElementById('krakow')
+var krkjson = requestWeater(krk, "Krakow,pl")	;
+var norden = document.getElementById('norden')
+var nordenjson = requestWeater(norden, "Norden,de");
+}
