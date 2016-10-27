0bd0fc92cad5e0b53af5cf076fffc80fc2796582
MrMargouillat <klaja.raphael@gmail.com>
2016-10-27 10:47:41 +0200

    First my first com"

--- index.php
+++ index.php
@@ -1,317 +1,35 @@
 <!DOCTYPE html>
-<html lang="en">
-    <head>
-        <title></title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="css/style.css" rel="stylesheet">
-        <link href="style.css" rel="stylesheet">
-    </head>
-    <body>
-        <div class="site-cache"></div>
-        <header>
-            <div class="menu_icon" id="btn_menu">&#xe900;</div>
-            <div class="user_icon" id="btn_user">&#xe902;</div>
-           <div id="logo"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/148866/cd-logo.svg" alt="Homepage"></div>
-           <div class="leftMenu">
-               <nav>
-                   <ul class="listMenu">
-                       <li class="linkListMenu">
-                           <a href="#" class="">Accueil</a>
-                       </li>
-                       <li class="linkListMenu">
-                           <a href="#" class="">Compte</a>
-                       </li>
-                       <li class="linkListMenu">
-                           <a href="#" class="">A propos</a>
-                       </li>
-                   </ul>
-               </nav>
-           </div>
-           <div class="rightMenu">
-               <div class="user">
-                   <div class="userImg">&#xe902;</div>
-                   <div class="myaccount">
-                       <a href="#" class="btn-important">Mon Compte</a>
-                   </div>
-               </div>
-           </div>
-       </header>
-       
-           
-    <div class="container">
-       <div class="listProducts">
-            <ul id="cd-gallery-items" class="cd-container">
-                <li class="itemApp">
-                    <div class="app">
-                        <div class="imgApp">
-                            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/148866/thumb.jpg" alt="Preview image">
-                        </div>
-                        <a class="cache-image"></a>
-                        <div class="titleApp">
-                            Ma super app
-                        </div>
-                    </div>
-                </li>
-                <li class="itemApp">
-                    <div class="app">
-                        <div class="imgApp">
-                            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/148866/thumb.jpg" alt="Preview image">
-                        </div>
-                        <a class="cache-image"></a>
-                        <div class="titleApp">
-                            Ma super app
-                        </div>
-                    </div>
-                </li>
-                <li class="itemApp">
-                    <div class="app">
-                        <div class="imgApp">
-                            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/148866/thumb.jpg" alt="Preview image">
-                        </div>
-                        <a class="cache-image"></a>
-                        <div class="titleApp">
-                            Ma super app
-                        </div>
-                    </div>
-                </li>
-                <li class="itemApp">
-                    <div class="app">
-                        <div class="imgApp">
-                            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/148866/thumb.jpg" alt="Preview image">
-                        </div>
-                        <a class="cache-image"></a>
-                        <div class="titleApp">
-                            Ma super app
-                        </div>
-                    </div>
-                </li>
-                <li class="itemApp">
-                    <div class="app">
-                        <div class="imgApp">
-                            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/148866/thumb.jpg" alt="Preview image">
-                        </div>
-                        <a class="cache-image"></a>
-                        <div class="titleApp">
-                            Ma super app
-                        </div>
-                    </div>
-                </li>
-                
-                <li class="itemApp">
-                    <div class="app">
-                        <div class="imgApp">
-                            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/148866/thumb.jpg" alt="Preview image">
-                        </div>
-                        <a class="cache-image"></a>
-                        <div class="titleApp">
-                            Ma super app
-                        </div>
-                    </div>
-                </li>
-                <li class="itemApp">
-                    <div class="app">
-                        <div class="imgApp">
-                            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/148866/thumb.jpg" alt="Preview image">
-                        </div>
-                        <a class="cache-image"></a>
-                        <div class="titleApp">
-                            Ma super app
-                        </div>
-                    </div>
-                </li>
-                <li class="itemApp">
-                    <div class="app">
-                        <div class="imgApp">
-                            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/148866/thumb.jpg" alt="Preview image">
-                        </div>
-                        <a class="cache-image"></a>
-                        <div class="titleApp">
-                            Ma super app
-                        </div>
-                    </div>
-                </li>
-                <li class="itemApp">
-                    <div class="app">
-                        <div class="imgApp">
-                            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/148866/thumb.jpg" alt="Preview image">
-                        </div>
-                        <a class="cache-image"></a>
-                        <div class="titleApp">
-                            Ma super app
-                        </div>
-                    </div>
-                </li>
-                <li class="itemApp">
-                    <div class="app">
-                        <div class="imgApp">
-                            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/148866/thumb.jpg" alt="Preview image">
-                        </div>
-                        <a class="cache-image"></a>
-                        <div class="titleApp">
-                            Ma super app
-                        </div>
-                    </div>
-                </li>
-                <li class="itemApp">
-                    <div class="app">
-                        <div class="imgApp">
-                            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/148866/thumb.jpg" alt="Preview image">
-                        </div>
-                        <a class="cache-image"></a>
-                        <div class="titleApp">
-                            Ma super app
-                        </div>
-                    </div>
-                </li>
-                <li class="itemApp">
-                    <div class="app">
-                        <div class="imgApp">
-                            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/148866/thumb.jpg" alt="Preview image">
-                        </div>
-                        <a class="cache-image"></a>
-                        <div class="titleApp">
-                            Ma super app
-                        </div>
-                    </div>
-                </li>
-                <li class="itemApp">
-                    <div class="app">
-                        <div class="imgApp">
-                            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/148866/thumb.jpg" alt="Preview image">
-                        </div>
-                        <a class="cache-image"></a>
-                        <div class="titleApp">
-                            Ma super app
-                        </div>
-                    </div>
-                </li>
-                <li class="itemApp">
-                    <div class="app">
-                        <div class="imgApp">
-                            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/148866/thumb.jpg" alt="Preview image">
-                        </div>
-                        <a class="cache-image"></a>
-                        <div class="titleApp">
-                            Ma super app
-                        </div>
-                    </div>
-                </li>
-                <li class="itemApp">
-                    <div class="app">
-                        <div class="imgApp">
-                            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/148866/thumb.jpg" alt="Preview image">
-                        </div>
-                        <a class="cache-image"></a>
-                        <div class="titleApp">
-                            Ma super app
-                        </div>
-                    </div>
-                </li>
-                <li class="itemApp">
-                    <div class="app">
-                        <div class="imgApp">
-                            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/148866/thumb.jpg" alt="Preview image">
-                        </div>
-                        <a class="cache-image"></a>
-                        <div class="titleApp">
-                            Ma super app
-                        </div>
-                    </div>
-                </li>
-                <li class="itemApp">
-                    <div class="app">
-                        <div class="imgApp">
-                            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/148866/thumb.jpg" alt="Preview image">
-                        </div>
-                        <a class="cache-image"></a>
-                        <div class="titleApp">
-                            Ma super app
-                        </div>
-                    </div>
-                </li>
-                <li class="itemApp">
-                    <div class="app">
-                        <div class="imgApp">
-                            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/148866/thumb.jpg" alt="Preview image">
-                        </div>
-                        <a class="cache-image"></a>
-                        <div class="titleApp">
-                            Ma super app
-                        </div>
-                    </div>
-                </li>
-                <li class="itemApp">
-                    <div class="app">
-                        <div class="imgApp">
-                            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/148866/thumb.jpg" alt="Preview image">
-                        </div>
-                        <a class="cache-image"></a>
-                        <div class="titleApp">
-                            Ma super app
-                        </div>
-                    </div>
-                </li>
-                <li class="itemApp">
-                    <div class="app">
-                        <div class="imgApp">
-                            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/148866/thumb.jpg" alt="Preview image">
-                        </div>
-                        <a class="cache-image"></a>
-                        <div class="titleApp">
-                            Ma super app
-                        </div>
-                    </div>
-                </li>
-                <li class="itemApp">
-                    <div class="app">
-                        <div class="imgApp">
-                            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/148866/thumb.jpg" alt="Preview image">
-                        </div>
-                        <a class="cache-image"></a>
-                        <div class="titleApp">
-                            Ma super app
-                        </div>
-                    </div>
-                </li>
-                <li class="itemApp">
-                    <div class="app">
-                        <div class="imgApp">
-                            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/148866/thumb.jpg" alt="Preview image">
-                        </div>
-                        <a class="cache-image"></a>
-                        <div class="titleApp">
-                            Ma super app
-                        </div>
-                    </div>
-                </li>
-                <li class="itemApp">
-                    <div class="app">
-                        <div class="imgApp">
-                            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/148866/thumb.jpg" alt="Preview image">
-                        </div>
-                        <a class="cache-image"></a>
-                        <div class="titleApp">
-                            Ma super app
-                        </div>
-                    </div>
-                </li>
-                <li class="itemApp">
-                    <div class="app">
-                        <div class="imgApp">
-                            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/148866/thumb.jpg" alt="Preview image">
-                        </div>
-                        <a class="cache-image"></a>
-                        <div class="titleApp">
-                            Ma super app
-                        </div>
-                    </div>
-                </li>
-                
-            </ul>
-       </div>
-    </div>
-    <script src="js/app.js">
-    </script>
-    </body>
+<html lang="fr">
+	<head>
+		<title>Website</title>
+		<meta charset="UTF-8">
+		
+		<meta content="width=device-width, initial-scale=0.25, maximum-scale=0.25,minimum-scale=0.25, user-scalable=0" name="viewport">
+
+		<link href="css/style.css" rel="stylesheet">
+	</head>
+	<body class="hideOverflow">
+		<header>
+			<nav class="nav">
+				<a class="btnNav" href="index.html">home</a>
+				<a class="btnNav" href="about.html">my work</a>
+				<a class="btnNav language_menu" href="#" id="header__language" >language</a>
+					<ul class="nav_child">
+						<li><a href="">English</a></li>
+						<li><a href="">French</a></li>
+						<li><a href="">German</a></li>
+					</ul>
+			</nav>
+		</header>
+			<div class="imgBackStanding"></div>
+			<h1 class="welcomeMsg">
+				<p>Welcome to Website,</p>
+				<p>the portfolio of a </p>
+				<p>16-year-old coder</p>
+			</h1>
+		<footer>
+			<p class="pCredits">Big thanks to <strong class="credits">Héloise Guillot</strong></p>
+		</footer>
+		<script src="./js/app.js"></script>
+	</body>
 </html>
--- js/app.js
+++ js/app.js
@@ -1,30 +1,6 @@
-var navBtn  = document.querySelector('#btn_menu')
-var siteCache  = document.querySelector('.site-cache')
-var userBtn  = document.querySelector('#btn_user')
-
-
+var navBtn  = document.querySelector('#header__language')
 navBtn.addEventListener("click", function(e){
     e.preventDefault();
     console.log(document.querySelector('body'));
-    document.querySelector('body').classList.toggle('with--sidebar--left');
-    document.querySelector('body').classList.remove('with--sidebar--right');
-
+    document.querySelector('body').classList.toggle('with--sidebar');
 });
-
-siteCache.addEventListener("click", function(e){
-    e.preventDefault();
-    console.log(document.querySelector('body'));
-    document.querySelector('body').classList.remove('with--sidebar--left');
-    document.querySelector('body').classList.remove('with--sidebar--right');
-});
-
-
-
-
-userBtn.addEventListener("click", function(e){
-    e.preventDefault();
-    console.log(document.querySelector('body'));
-    document.querySelector('body').classList.toggle('with--sidebar--right');
-    document.querySelector('body').classList.remove('with--sidebar--left');
-});
-
